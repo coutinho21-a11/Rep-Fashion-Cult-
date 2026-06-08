f7b2d70bbbef590b44ee61a880bd85fc7a3a8c89
coutinho21-a11 <coutinhojenny1@gmail.com>
2026-06-08 05:20:05 +0100

    Add Firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configuration Firebase locale - ne pas commiter sur GitHub
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBDOUWrEZkF48YiJA_z4W21Oj-3d5KdiU0",
+  authDomain: "rep-fashion-cult.firebaseapp.com",
+  projectId: "rep-fashion-cult",
+  storageBucket: "rep-fashion-cult.firebasestorage.app",
+  messagingSenderId: "682165033201",
+  appId: "1:682165033201:web:8646403cb0d644edc9f150",
+  measurementId: "G-6GZ32CWHWM"
+};
